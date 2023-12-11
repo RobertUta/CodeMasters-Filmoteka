25a6ea3fa60bbe2c0171121fd5a93705e2e7027e
RobertUta <roy_robert_99@yahoo.com>
2023-12-11 13:39:04 +0200

    update paginare
    .

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -2,11 +2,13 @@ import axios from 'axios';
 import apiMovie from './api-movie.js';
 import { getMovieGenres } from './api-genres.js';
 
-const ITEMS_PER_PAGE = 20; 
-
+const ITEMS_PER_PAGE = 20;
+let currentPage = 1;
+let totalPages = 1; // Declare totalPages globally
 
 async function displayMovieCards(movies) {
   const movieContainer = document.querySelector('.movie-container');
+  movieContainer.innerHTML = '';
 
   try {
     const genres = await getMovieGenres();
@@ -20,7 +22,7 @@ async function displayMovieCards(movies) {
       movieImage.src = imageUrl;
       movieImage.alt = movie.title;
       movieImage.classList.add('movie-image');
-      movieImage.tabIndex = 0; 
+      movieImage.tabIndex = 0;
 
       const movieTitle = document.createElement('h3');
       movieTitle.textContent = movie.title;
@@ -49,12 +51,17 @@ async function displayMovieCards(movies) {
   }
 }
 
-
-async function getMoviesFromApi() {
+async function getMoviesFromApi(page) {
   try {
-    const response = await axios.request(apiMovie);
+    const response = await axios.request({
+      ...apiMovie,
+      params: {
+        ...apiMovie.params,
+        page: page || 1,
+      },
+    });
 
-    if (!response.status === 200) {
+    if (response.status !== 200) {
       throw new Error('Network response was not ok');
     }
 
@@ -65,15 +72,110 @@ async function getMoviesFromApi() {
   }
 }
 
+async function getTotalMoviesCount() {
+  try {
+    const response = await axios.request(apiMovie);
+
+    if (response.status !== 200) {
+      throw new Error('Network response was not ok');
+    }
+
+    return response.data.total_results;
+  } catch (error) {
+    console.error('Error fetching total movie count:', error);
+    return 0;
+  }
+}
+
+async function displayPaginationNumbers() {
+  const pageNumbersContainer = document.getElementById('pageNumbers');
+  pageNumbersContainer.innerHTML = '';
+
+  const totalMovies = await getTotalMoviesCount();
+  totalPages = Math.ceil(totalMovies / ITEMS_PER_PAGE); // Update totalPages globally
+
+  const maxDisplayedPages = 5;
+  const maxEllipsisGap = 2;
+
+  let startPage = Math.max(1, currentPage - Math.floor(maxDisplayedPages / 2));
+  let endPage = Math.min(totalPages, startPage + maxDisplayedPages - 1);
+
+  if (totalPages > maxDisplayedPages) {
+    if (endPage - startPage < maxDisplayedPages - 1) {
+      startPage = Math.max(1, endPage - maxDisplayedPages + 1);
+    }
+
+    if (startPage > 1) {
+      const firstPageButton = document.createElement('button');
+      firstPageButton.textContent = '1';
+      firstPageButton.addEventListener('click', () => goToPage(1));
+      pageNumbersContainer.appendChild(firstPageButton);
+
+      if (startPage > 2) {
+        const ellipsisStart = document.createElement('span');
+        ellipsisStart.textContent = '...';
+        pageNumbersContainer.appendChild(ellipsisStart);
+      }
+    }
+  }
+
+  for (let i = startPage; i <= endPage; i++) {
+    const pageNumberButton = document.createElement('button');
+    pageNumberButton.textContent = i;
+    pageNumberButton.addEventListener('click', () => goToPage(i));
+    pageNumbersContainer.appendChild(pageNumberButton);
+  }
+
+  if (totalPages > maxDisplayedPages && endPage < totalPages) {
+    if (totalPages - endPage > maxEllipsisGap) {
+      const ellipsisEnd = document.createElement('span');
+      ellipsisEnd.textContent = '...';
+      pageNumbersContainer.appendChild(ellipsisEnd);
+    }
+
+    const lastPageButton = document.createElement('button');
+    lastPageButton.textContent = totalPages;
+    lastPageButton.addEventListener('click', () => goToPage(totalPages));
+    pageNumbersContainer.appendChild(lastPageButton);
+  }
+}
 
 async function displayMovieCardsFromApi() {
   try {
-    const movies = await getMoviesFromApi();
+    const movies = await getMoviesFromApi(currentPage);
     await displayMovieCards(movies);
   } catch (error) {
     console.error('There was a problem displaying movie cards:', error);
   }
 }
 
+function goToPage(pageNumber) {
+  currentPage = pageNumber;
+  displayPageData();
+}
 
-displayMovieCardsFromApi();
+async function displayPageData() {
+  await displayMovieCardsFromApi();
+  displayPaginationNumbers();
+}
+
+// Next and Previous Buttons
+const nextButton = document.getElementById('nextPageButton');
+const prevButton = document.getElementById('prevPageButton');
+
+nextButton.addEventListener('click', () => {
+  if (currentPage < totalPages) {
+    currentPage++;
+    displayPageData();
+  }
+});
+
+prevButton.addEventListener('click', () => {
+  if (currentPage > 1) {
+    currentPage--;
+    displayPageData();
+  }
+});
+
+// Example usage:
+displayPageData();
